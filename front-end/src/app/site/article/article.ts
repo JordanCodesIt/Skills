e7926a777fbd7167b1ca25a4842db528a194d31b
Aymen-drid <aymnehocam@gmail.com>
2025-10-26 17:30:47 +0100

    Update article component logic

--- front-end/src/app/site/article/article.ts
+++ front-end/src/app/site/article/article.ts
@@ -2,45 +2,48 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
-interface Tag{
-  name:string;
+interface Tag {
+  name: string;
 }
 @Component({
   selector: 'app-post-card',
-  imports: [CommonModule, MatIconModule,],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './article.html',
-  styleUrls: ['./article.scss']
+  styleUrls: ['./article.scss'],
 })
 export class PostCardComponent {
   // @Input() category!: Tag ;
-  constructor(private router:Router ){}
+  constructor(private router: Router) {}
   @Input() authorName: string = 'Sudharsan Saravanan';
   @Input() postDate: string = 'Oct 21';
   @Input() title: string = 'My Hacktoberfest 2025 Journey: Lessons from 14 Accepted PRs';
   @Input() tags!: Tag[];
   @Input() readTime: number = 2;
   @Input() isBookmarked: boolean = false;
-
+  @Input() commentsCount!: number;
+  @Input() viewsCount!: number;
+  @Input() reactionsCount!: number;
   @Output() commentClick = new EventEmitter<void>();
   @Output() bookmarkClick = new EventEmitter<boolean>();
   @Output() cardClick = new EventEmitter<void>();
   getInitials(name: string): string {
     return name
       .split(' ')
-      .map(word => word[0])
+      .map((word) => word[0])
       .join('')
       .toUpperCase()
       .substring(0, 2);
   }
-
-  onAddComment() {
+  onAddComment(event: Event) {
+    event.stopPropagation();
     this.commentClick.emit();
   }
-  onCardClick(){
+  onCardClick() {
     this.cardClick.emit();
   }
 
-  onBookmark() {
+  onBookmark(event:Event) {
+    event.stopPropagation()
     this.isBookmarked = !this.isBookmarked;
     this.bookmarkClick.emit(this.isBookmarked);
   }
