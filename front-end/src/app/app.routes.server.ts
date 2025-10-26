2217170bce9a452bf6afe58f3f33af7a5fb02a3d
Aymen-drid <aymnehocam@gmail.com>
2025-10-26 18:44:00 +0100

    fix build opitons

--- front-end/src/app/app.routes.server.ts
+++ front-end/src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {path:'articles/*',
+    renderMode:RenderMode.Client
+  } ,
   {
     path: '**',
     renderMode: RenderMode.Prerender
