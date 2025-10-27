77f01697906702eba2e5c2b253c4b83276ffb95d
Aymen-drid <aymnehocam@gmail.com>
2025-10-27 16:42:10 +0100

    switch to https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../../cert/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../../cert/cert.pem')),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.enableCors({
     origin: ['https://skill-hub-version1.vercel.app'],
+
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
       'Content-Type',
       'Authorization',
       'apollo-require-preflight',
     ],
   });
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
+
 bootstrap();
